d5b13136769b58e827b420f2c35b442591bd316e
Rui Zhong <48166604+sourcefull@users.noreply.github.com>
2020-02-16 21:25:22 -0800

    Initial Commit

---
+++ testapp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  testapp
+//
+//  Created by Rick Zhong on 2/16/20.
+//  Copyright © 2020 Rick Zhong. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
